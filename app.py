4a0cca4a03423201222e5e76f27486f2a6dccb44
EhtashamAli <ehtasham.ali1979@gmail.com>
2019-06-12 15:01:26 +0500

    update

--- app.py
+++ app.py
@@ -43,4 +43,4 @@ def predict():
     return jsonify(y)
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=3000, debug=True, threaded=True)
+    app.run(host="0.0.0.0", port=3000, debug=False, threaded=True)
