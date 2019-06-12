fb8e268ffebf4c379dbcc59706b202c380a295b9
EhtashamAli <ehtasham.ali1979@gmail.com>
2019-06-12 14:58:53 +0500

    added cors

--- app.py
+++ app.py
@@ -2,7 +2,11 @@ import numpy as np
 from flask import Flask, request, jsonify
 import pickle
 import json
+from flask_cors import CORS
+
+
 app = Flask(__name__)
+CORS(app)
 
 linear_model = pickle.load(open('./Model/model_linear_model.pkl','rb'))
 naive_bayes = pickle.load(open('./Model/model_naive_bayes.pkl','rb'))
@@ -39,4 +43,4 @@ def predict():
     return jsonify(y)
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8080, debug=True, threaded=True)
+    app.run(host="0.0.0.0", port=3000, debug=True, threaded=True)
